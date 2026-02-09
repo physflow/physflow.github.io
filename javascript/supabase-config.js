b36b617cdb762801421c4c4713f1e8d2b7687d36
physflow <physflow.devs@gmail.com>
2026-02-09 23:38:09 +0600

    Update supabase-config.js

--- javascript/supabase-config.js
+++ javascript/supabase-config.js
@@ -2,5 +2,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabaseUrl = 'https://hmzcipbchhsdycgozhzd.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtemNpcGJjaGhzZHljZ296aHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1ODIzNDksImV4cCI6MjA4NjE1ODM0OX0.5rMQnPHo6haSnJwdagPOt9c4MnLWJWKx5gk8eKRO66A'
-
+ 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
