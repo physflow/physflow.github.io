03866e9ca5d800317f771063324740f7ff900d15
physflow <physflow.devs@gmail.com>
2026-02-11 23:13:28 +0600

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -56,8 +56,8 @@ const createQuestionCard = (question) => {
                         <span class="text-[11px] text-gray-500">উত্তর</span>
                     </div>
                     <div class="flex items-center gap-1">
-                        <span class="text-[14px] font-medium text-gray-400">${toBanglaNumber(question.views || 0)}</span>
-                        <span class="text-[11px] text-yellow-500">দেখেছে</span>
+                        <span class="text-[14px] font-medium text-yellow-500">${toBanglaNumber(question.views || 0)}</span>
+                        <span class="text-[11px] text-gray-500">দেখেছে</span>
                     </div>
                 </div>
                 
